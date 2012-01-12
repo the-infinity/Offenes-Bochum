8a23ec69f0dbc947795aeac270c2cc4627c05414
Ernesto Ruge <mail@ernestoruge.de>
2012-01-12 16:16:35 +0100

    model for openspending web app

---
+++ model.js
@@ -0,0 +1,32 @@
+{
+  "amount": {
+    "column": "amount",
+    "datatype": "float",
+    "type": "measure",
+    "description": null,
+    "label": "Amount"
+  },
+  "time": {
+    "description": null,
+    "format": null,
+    "datatype": "date",
+    "label": "Time",
+    "column": "year",
+    "type": "date"
+  },
+  "mapping": {
+    "description": null,
+    "datatype": "id",
+    "label": "Mapping",
+    "column": "object_id",
+    "key": true,
+    "type": "attribute"
+  },
+  "label": {
+    "column": "object_descr",
+    "datatype": "string",
+    "type": "attribute",
+    "description": null,
+    "label": "Label"
+  }
+}
